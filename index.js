9221ee78a8a0b4cc4c788eb794e4c9ac315833eb
Mykyta Milshyn <m.milshyn.freelancer@gmail.com>
2024-11-26 01:30:56 +0200

    add index.js with express infrastructure

--- index.js
+++ index.js
@@ -1,22 +1,47 @@
-const http = require('http');
-const port = 3000;
+const express = require('express');
+const app = express();
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    const url = req.url;
-    if (url === '/about') {
-        res.write('<h1>about us page<h1>');
-        res.end();
-    } else if (url === '/contact') {
-        res.write('<h1>contact us page<h1>');
-        res.end();
+const products = [
+    { id: 1, name: 'Product 1', brand: 'Brand A' },
+    { id: 2, name: 'Product 2', brand: 'Brand B' },
+    { id: 3, name: 'Product 3', brand: 'Brand A' },
+];
+
+app.get('/', (request, response) => {
+    response.send('Response for GET request');
+});
+
+app.get('/products/brand/:brand', (req, res) => {
+    const { brand } = req.params;
+    const filteredProducts = products.filter(product => product.brand === brand);
+    if (filteredProducts) {
+        res.json(filteredProducts);
     } else {
-        res.write('<h1>Hello World!<h1>');
-        res.write('<h2>My name Mykyta<h2>');
-        res.end();
+        res.status(404).send({ error: 'Product by brand not found' });
     }
 });
 
-server.listen(port, () => {
-    console.log(`server start at http://localhost:${port}/`);
+app.get('/products/name/:name', (req, res) => {
+    const { name } = req.params;
+    const filteredProducts = products.filter(product => product.name === name);
+    if (filteredProducts) {
+        res.json(filteredProducts);
+    } else {
+        res.status(404).send({ error: 'Product by name not found' });
+    }
+});
+
+app.get('/products/id/:id', (req, res) => {
+    const { id } = req.params;
+    const filteredProducts = products.find(product => product.id === parseInt(id, 10));
+    if (product) {
+        res.json(filteredProducts);
+    } else {
+        res.status(404).send({ error: 'Product by id not found' });
+    }
+});
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server start at http://localhost:${port}/`);
 });
